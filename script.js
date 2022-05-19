97c7a5341c6c8ee2d2fddaf7deac48726e73a691
Lim Qiao En <92434807+qiaoen17@users.noreply.github.com>
2022-05-19 23:25:26 +0800

    Stickman added
    Added style for buttons, body, and drew a stickman. Haven't figured out why the stickman in js doesnt work pls send help....

---
+++ script.js
@@ -0,0 +1,24 @@
+canvas = function() {
+
+    hangman = document.getElementById("Hangman");
+    ctx = hangman.getContext("2d");
+    ctx.beginPath();
+    ctx.strokeStyle = "#fff";
+    context.lineWidth = 4;
+
+};
+
+head = function() {
+    headHangman = document.getElementById("Hangman");
+    ctx = headHangman.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(50, 50, 40, 0, 2 * Math.PI, false);
+    ctx.stroke();
+}
+
+frame1 = function() {
+    draw(0, 150, 150, 150)
+}
+
+
+
