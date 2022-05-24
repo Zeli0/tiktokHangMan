54feabb3d2585d03eface2208ab6bad36969f7d4
Elijah Ku <69575234+Zeli0@users.noreply.github.com>
2022-05-24 18:37:12 +0800

    Update index.js

--- hangman-react/src/index.js
+++ hangman-react/src/index.js
@@ -1,95 +1,144 @@
-import React from 'react';
+import React, {useState, createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 //import Letter from "./Letter.js";
 //import Blank from "./Blank.js";
 
-var abcde = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+var abcde = ["a", "b", "c", "d", "e", "f", "g", "h",
+"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+"t", "u", "v", "w", "x", "y", "z"];
 
-var splitWord;
 var word;
+const userContext = createContext();
 
-class Letter extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = { 
-          abc: props.abc,
-          disabled: false
-      };
+
+function Letter(props) {
+  const [disabled, setDisabled] = useState(false);
+  const [abc, setABC] = useState(props.abc);
+
+  const handleClick = () => {
+    if (disabled) {
+        return;
+    }
+    setDisabled(true);
+    console.log("Asdf");
+    props.parentCallBack(abc);
+    // Send     
   }
 
-  handleClick = (event) => {
-      if (this.state.disabled) {
-          return;
-      }
-      this.setState({disabled: true});
-      console.log("Asdf");
-      if (word.includes(this.state.abc)) {
-        for (let i = 0; i < splitWord.length; i++) {
-          if(this.state.abc == splitWord[i]) {
-            let temp = useRef(i);
-            temp.setState({ displayLetter: temp.state.realLetter });;
-          }
-        }
-      }
-      // Send     
-  }
+  return (
+    <button onClick={handleClick} disabled={disabled}>
+        {abc}
+    </button>
+);
 
-  render() {
-      return (
-          <button onClick={this.handleClick} disabled={this.state.disabled}>
-              {this.state.abc}
-          </button>
-      );
-  }
 }
 
-class Blank extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = { 
-          realLetter: props.realLetter,
-          displayLetter: "_" 
-      };
+function Blanks(props) {
+  const [realWord, setRealWord] = useState(props.realWord.split(""));
+  const [displayWord, setDisplayWord] = useState(realWord.map((char) => "_"));
+
+  const changeVis = (letter) => {
+    console.log("sothony sadtano");
+    let isCorrect = false;
+    let temp = displayWord.map((item) => item);
+    for (let i = 0; i < realWord.length; i++) {
+      if (realWord[i] == letter) {        
+        temp[i] = letter;
+        isCorrect = true;
+      }
+    }
+    if (isCorrect) {
+      setDisplayWord(temp);
+    } else {
+      props.murder();
+    }
   }
 
-  render() {
-      return (
-          <div class="box">{this.state.displayLetter}</div>
-      );
-  }
+  props.parentCallBack(changeVis);
+  return displayWord.map((item) => {
+    return <div class="box">{item}</div>
+  });
 }
 
-function Alphabet() {
+
+function Alphabet(props) {
   var count = -1;
+  const handleCallback = (childData) => {
+    props.parentCallBack(childData);
+  }
   return abcde.map((item) => {
     count++;
-    return <Letter abc={item} key={count} />
+    return <Letter abc={item} key={count} parentCallBack={handleCallback}/>
   })
 }
 
-function GenerateWord() {
+/*function Gather() {
   word = "abcdcba";
-  var count = -1;
-  splitWord = word.split("");
-  return splitWord.map((item) => {
-    count++;
-    return <Blank realLetter={item} ref={count} />
-  })
-}
+  const [changeVis, setChangeVis] = useState(undefined);
+  const [lives, setLives] = useState(10);
 
-function Gather() {
+  const handleCallback = (childData) => {
+    changeVis(childData);
+  }
+
+  const edit = (childData) => {
+    setChangeVis(childData);
+  }
+  const updateLife = () => {
+    setLives(lives - 1);
+  }
+
   return (
     <div>
+      <h3>You have {lives} lives left.</h3>
       <div class="flex align-start justify-start">
-        <GenerateWord />
+        <Blanks  realWord={word} parentCallBack={edit} murder={updateLife}/>
       </div>
       <div>
-        <Alphabet />
+        <Alphabet parentCallBack={handleCallback} />
       </div>
     </div>
-  );
+    )
+} */
+
+
+class Gather extends React.Component {
+  constructor(props) {
+    super(props);
+    word = "abcdcba";
+    this.state = {
+      changeVis : undefined,
+      lives : 10
+    }
+  }
+
+  handleCallback = (childData) => {
+    this.state.changeVis(childData);
+  }
+
+  edit = (childData) => {
+    this.state.changeVis = childData;
+  }
+
+  updateLife = () => {
+    let temp = this.state.lives;
+    console.log(temp);
+    this.setState({lives : temp - 1});
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>You have {this.state.lives} lives left.</h1>
+      <div class="flex align-start justify-start">
+        <Blanks  realWord={word} parentCallBack={this.edit} murder={this.updateLife}/>
+      </div>
+      <div>
+        <Alphabet parentCallBack={this.handleCallback} />
+      </div>
+    </div>
+    )
+  }
 }
 const root = ReactDOM.createRoot(document.getElementById('amogus'));
 root.render(<Gather />);
